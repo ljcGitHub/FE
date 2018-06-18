4e70f3fcd75df3c9daeb9b5ffe357604963152cd
ljcGitHub <523252797@qq.com>
2018-06-18 08:17:52 +0800

    eidt user password is 123456

--- example/fifth/database/data-init/user.js
+++ example/fifth/database/data-init/user.js
@@ -3,7 +3,7 @@ export const user = [
   {
     id: 101,
     account: 'beiji',
-    password: '$2a$10$KTibVOcxQqAe19951NJfBe11qJBFPwRKPrprpcaMPB5a63/BHpBkW',
+    password: '$2a$10$7kKcGG94P2WlBMB49fAmluyJgky0F8sopeDLVYde25ueO3V1J2pNu',
     name: '张三',
     portraits: '',
     note: ''
